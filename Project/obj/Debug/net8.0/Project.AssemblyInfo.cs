bc3d7993aebb83056c7b1ad478a21e40a6322646
Cell136 <111261525+Cell136@users.noreply.github.com>
2024-05-31 13:03:19 +0700

    login/logout
    Chua co Model , chua add-migration intialCreate, chua phan quyen

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4119bdf4b518ec943d14ae2d1ff7300cccea3a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f56db83aa939ab3e183712349fdf6e897505d5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
